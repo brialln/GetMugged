a3cbcbe92823d54ae38ee8304d37b7c7ab2ca5f2
Brian Allen <brianallenlm913@gmail.com>
2023-04-20 17:57:31 +0800

    first commit

---
+++ src/data.js
@@ -0,0 +1,34 @@
+let shopItemsData = [{
+    id: "anhdfjasn",
+    name: "Mudmug",
+    price: 13.13,
+    img: "picturettes/mug1.jpg"
+},
+ 
+{
+    id: "sandjsna",
+    name: "Muck",
+    price: 10.12,
+    img: "picturettes/mug2.jpg"
+}, 
+
+{
+    id: "asdnsand",
+    name: "Dinomug",
+    price: 13.00,
+    img: "picturettes/mug3.jpg"
+}, 
+
+{
+    id: "sndlks",
+    name: "Dug",
+    price: 11.13,
+    img: "picturettes/mug4.jpg"
+}, 
+
+{
+    id: "sadnjsand",
+    name: "Pengmug",
+    price: 14.00,
+    img: "picturettes/mug5.jpg"
+}]
